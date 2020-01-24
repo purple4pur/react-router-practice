6f91d3f46f2cd50aeb22083743c35e48d4740c0d
Purple4pur <purple4pur@gmail.com>
2020-01-24 15:16:58 +0800

    NEW: use components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import {
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import {
+  Home,
+  Article,
+  About
+} from './views'
 
 export default class App extends Component {
   render() {
     return (
-      <h1>
-        hello world
-      </h1>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">首页</Link>
+            </li>
+            <li>
+              <Link to="/article">文章</Link>
+            </li>
+            <li>
+              <Link to="/about">关于</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Article} path="/article" exact />
+          <Route component={About} path="/about" exact />
+        </Switch>
+      </>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route component={App} path="/" />
+  </Router>,
   document.querySelector('#root')
 )
